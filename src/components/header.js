a93aa031eca5a78cf45dfa656e44dfc44e8be43d
Trupesh Chapaneri <trupesh789@gmail.com>
2021-08-15 19:33:39 +0530

    Changes

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
-import {AppBar, Button, Toolbar, Typography} from '@material-ui/core'
-import {makeStyles} from '@material-ui/core/styles'
+import {AppBar, Button, Toolbar, Typography, useMediaQuery} from '@material-ui/core'
+import {makeStyles, useTheme} from '@material-ui/core/styles'
 import AddIcon from '@material-ui/icons/Add'
 import {AddModal} from './add-modal'
 
@@ -15,13 +15,15 @@ const useStyles = makeStyles(() => ({
 function Header() {
   const classes = useStyles()
   const [open, setOpen] = useState(false)
+  const theme = useTheme()
+  const xsScreen = useMediaQuery(theme.breakpoints.down('xs'))
 
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h6" className={classes.title}>
-            SERP (Search Engine Results Page)
+            SERP {!xsScreen && `(Search Engine Results Page)`}
           </Typography>
           <Button startIcon={<AddIcon />} color="inherit" onClick={() => setOpen(true)}>
             Add Keyword
