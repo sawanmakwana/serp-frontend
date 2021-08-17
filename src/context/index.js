795806396100e15e43771bdf06fb5e35681a0e5a
Trupesh Chapaneri <trupesh789@gmail.com>
2021-08-18 00:03:02 +0530

    Api call bug solved

--- src/context/index.js
+++ src/context/index.js
@@ -1,65 +1,63 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable no-console */
-import React, {useState} from 'react'
+import React from 'react'
 import {BrowserRouter as Router} from 'react-router-dom'
 import {QueryClient, QueryClientProvider, MutationCache, QueryCache} from 'react-query'
 import {ReactQueryDevtools} from 'react-query/devtools'
-import {Slide, Snackbar} from '@material-ui/core'
-import MuiAlert from '@material-ui/lab/Alert'
 
-function AppProviders({children}) {
-  const [toast, setToast] = useState(false)
-  const [toastmsg, setToastmsg] = useState('')
-  const [toastType, setToasttype] = useState('info')
+const queryCache = new QueryCache({
+  onError(error) {
+    console.log(error.response.data.message)
+    // setToast(true)
+    // setToasttype('error')
+    // setToastmsg(error.response.data.message)
+  },
+})
 
-  const queryCache = new QueryCache({
-    onError(error) {
-      console.log(error.response.data.message)
-      setToast(true)
-      setToasttype('error')
-      setToastmsg(error.response.data.message)
-    },
-  })
+const mutationCache = new MutationCache({
+  onError(error) {
+    console.log(error.response.data.message)
+    // setToast(true)
+    // setToasttype('error')
+    // setToastmsg(error.response.data.message)
+  },
+  onSuccess({data}) {
+    console.log(data.message)
+    // setToast(true)
+    // setToasttype('success')
+    // setToastmsg(data.message)
+  },
+})
 
-  const mutationCache = new MutationCache({
-    onError(error) {
-      console.log(error.response.data.message)
-      setToast(true)
-      setToasttype('error')
-      setToastmsg(error.response.data.message)
-    },
-    onSuccess({data}) {
-      console.log(data.message)
-      setToast(true)
-      setToasttype('success')
-      setToastmsg(data.message)
-    },
-  })
-
-  const queryClient = new QueryClient({
-    mutationCache,
-    queryCache,
-    defaultOptions: {
-      queries: {
-        useErrorBoundary: true,
-        refetchOnWindowFocus: false,
-        retry(failureCount, error) {
-          if (error.statusCode === 404) return false
-          if (failureCount < 2) return true
-          return false
-        },
+const queryClient = new QueryClient({
+  mutationCache,
+  queryCache,
+  defaultOptions: {
+    queries: {
+      useErrorBoundary: true,
+      refetchOnWindowFocus: false,
+      retry(failureCount, error) {
+        if (error.statusCode === 404) return false
+        if (failureCount < 2) return true
+        return false
       },
     },
-  })
+  },
+})
 
-  function TransitionUp(props) {
-    return <Slide {...props} direction="right" />
-  }
+function AppProviders({children}) {
+  // const [toast, setToast] = useState(false)
+  // const [toastmsg, setToastmsg] = useState('')
+  // const [toastType, setToasttype] = useState('info')
+
+  // function TransitionUp(props) {
+  //   return <Slide {...props} direction="right" />
+  // }
 
   return (
     <QueryClientProvider client={queryClient}>
       <Router>
-        <Snackbar
+        {/* <Snackbar
           open={toast}
           anchorOrigin={{
             vertical: 'bottom',
@@ -73,7 +71,7 @@ function AppProviders({children}) {
           <MuiAlert severity={toastType} elevation={6} variant="filled">
             {toastmsg}
           </MuiAlert>
-        </Snackbar>
+        </Snackbar> */}
         {children}
       </Router>
       <ReactQueryDevtools position="bottom-left" initialIsOpen={false} />
