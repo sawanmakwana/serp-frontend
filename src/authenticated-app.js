94a41530b3e92a2b3c319e7715584cfdeb925d68
Trupesh Chapaneri <trupesh789@gmail.com>
2021-08-28 15:57:30 +0530

    New dashboard added

--- src/authenticated-app.js
+++ src/authenticated-app.js
@@ -1,12 +1,16 @@
 import {Redirect, Route, Switch} from 'react-router-dom'
 import {AppLayout} from 'layouts/app-layout'
 import {Home} from 'pages/home'
+import {HomeNew} from 'pages/home_new'
 
 function AppRoutespath() {
   return (
     <Switch>
       <Route path="/home">
         <Home />
+      </Route>
+      <Route path="/dashboard">
+        <HomeNew />
       </Route>
 
       <Redirect to="/home" />
