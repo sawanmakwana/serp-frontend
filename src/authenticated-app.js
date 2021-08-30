245cd288dfc9c5e9ec51fdda8e6f48b8d75f6571
Trupesh Chapaneri <trupesh789@gmail.com>
2021-08-30 14:15:51 +0530

    New Layout added

--- src/authenticated-app.js
+++ src/authenticated-app.js
@@ -2,18 +2,23 @@ import {Redirect, Route, Switch} from 'react-router-dom'
 import {AppLayout} from 'layouts/app-layout'
 import {Home} from 'pages/home'
 import DashboardLayout from 'pages/DashboardLayout'
+import {HomeNewest} from 'pages/home-newest'
+import {AppLayoutNew} from './layouts/app-layout-new'
 
 function AppRoutespath() {
   return (
     <Switch>
-      <Route path="/home">
+      {/* <Route path="/home">
         <Home />
-      </Route>
-      <Route path="/new_layout">
+      </Route> */}
+      {/* <Route path="/new_layout">
         <DashboardLayout />
+      </Route> */}
+      <Route path="/">
+        <HomeNewest />
       </Route>
 
-      <Redirect to="/new_layout" />
+      <Redirect to="/" />
     </Switch>
   )
 }
@@ -23,7 +28,9 @@ function AuthenticatedApp() {
     // <AppLayout>
     //   <AppRoutespath />
     // </AppLayout>
-    <AppRoutespath />
+    <AppLayoutNew>
+      <AppRoutespath />
+    </AppLayoutNew>
   )
 }
 
---
+++ src/pages/home-newest.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HomeNewest() {
+  return (
+    <>
+      <h1>HomeNewest</h1>
+    </>
+  )
+}
+
+export {HomeNewest}
