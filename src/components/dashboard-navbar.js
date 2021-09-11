0d7ab6046d44bcb4848a857687110c9044ef01ba
Trupesh Chapaneri <trupesh789@gmail.com>
2021-09-11 16:08:17 +0530

    Unuse Removed

--- src/components/dashboard-navbar.js
+++ src/components/dashboard-navbar.js
@@ -1,14 +1,9 @@
-import {AppBar, Box, Hidden, IconButton, Toolbar, Typography, Button, useMediaQuery} from '@material-ui/core'
+import {AppBar, Box, Hidden, IconButton, Toolbar, Typography, useMediaQuery} from '@material-ui/core'
 import MenuIcon from '@material-ui/icons/Menu'
 import PropTypes from 'prop-types'
-import AddIcon from '@material-ui/icons/Add'
 import {useTheme} from '@material-ui/core/styles'
-import {useState} from 'react'
-import {useLocation} from 'react-router-dom'
-import {AddProjectListModal} from './add-project-list'
 
 const DashboardNavbar = ({onMobileNavOpen}) => {
-  const {pathname} = useLocation()
   const theme = useTheme()
   const xsScreen = useMediaQuery(theme.breakpoints.down('xs'))
 
@@ -17,12 +12,6 @@ const DashboardNavbar = ({onMobileNavOpen}) => {
       <Toolbar>
         <Typography variant="h5">SERP {!xsScreen && `(Search Engine Results Page)`}</Typography>
         <Box sx={{flexGrow: 1}} />
-        {/* {pathname === '/project' && (
-          <Button startIcon={<AddIcon />} color="inherit" onClick={() => setOpen(true)}>
-            Add Project
-          </Button>
-        )} */}
-
         <Hidden lgUp>
           <IconButton color="inherit" onClick={onMobileNavOpen}>
             <MenuIcon />
