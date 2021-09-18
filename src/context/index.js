a1cba32ab8d45994d48a34fefc7b03cbd5af914a
Trupesh Chapaneri <trupesh789@gmail.com>
2021-09-18 14:58:54 +0530

    React Devtool removed

--- src/context/index.js
+++ src/context/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import {BrowserRouter as Router} from 'react-router-dom'
 import {QueryClient, QueryClientProvider, MutationCache, QueryCache} from 'react-query'
-import {ReactQueryDevtools} from 'react-query/devtools'
 import {ToastContainer, toast} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import {AuthProvider} from './auth-context'
@@ -53,7 +52,7 @@ function AppProviders({children}) {
         />
         <AuthProvider>{children}</AuthProvider>
       </Router>
-      <ReactQueryDevtools position="bottom-left" initialIsOpen={false} />
+      {/* <ReactQueryDevtools position="bottom-left" initialIsOpen={false} /> */}
     </QueryClientProvider>
   )
 }
