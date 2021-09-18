a5f6bf634bdd6e4e30d59faaef94a3b3a51a7f37
Trupesh Chapaneri <trupesh789@gmail.com>
2021-09-18 14:52:43 +0530

    navBar name changes

--- src/components/dashboard-navbar.js
+++ src/components/dashboard-navbar.js
@@ -1,16 +1,12 @@
-import {AppBar, Box, Hidden, IconButton, Toolbar, Typography, useMediaQuery} from '@material-ui/core'
+import {AppBar, Box, Hidden, IconButton, Toolbar, Typography} from '@material-ui/core'
 import MenuIcon from '@material-ui/icons/Menu'
 import PropTypes from 'prop-types'
-import {useTheme} from '@material-ui/core/styles'
 
 const DashboardNavbar = ({onMobileNavOpen}) => {
-  const theme = useTheme()
-  const xsScreen = useMediaQuery(theme.breakpoints.down('xs'))
-
   return (
     <AppBar elevation={0}>
       <Toolbar>
-        <Typography variant="h5">SERP {!xsScreen && `(Search Engine Results Page)`}</Typography>
+        <Typography variant="h5">SERP</Typography>
         <Box sx={{flexGrow: 1}} />
         <Hidden lgUp>
           <IconButton color="inherit" onClick={onMobileNavOpen}>
